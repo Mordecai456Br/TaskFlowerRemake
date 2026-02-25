7a7d59dc74ce6034b27c9a8f2f5982566ef3bc45
Gabriel Freitas <g.albuquerque.seixas@accenture.com>
2026-02-25 11:45:31 -0300

    Fix/Rename: Removed enums export.

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,4 +5,4 @@ import { neon } from '@neondatabase/serverless';
 if(!process.env.DATABASE_URL) throw new Error('DATABASE_URL environment variable is required');
 
 const sql = neon(process.env.DATABASE_URL);
-export const index = drizzle(sql);
+export const neonDatabase = drizzle(sql);
