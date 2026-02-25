c405a4137b2f390cdaea8e1cedf52155ff6454ca
Gabriel Freitas <g.albuquerque.seixas@accenture.com>
2026-02-25 12:03:12 -0300

    Improvement: Adding new checks to avoid erros.

--- src/routes/helpers/queryStringFormater.ts
+++ src/routes/helpers/queryStringFormater.ts
@@ -1,11 +1,13 @@
 function formatQueryString(queryString: unknown): string | undefined {
     if(typeof queryString === 'string') {
-        return queryString.replace(/^"(.*)"$/, '$1')
+        return escapeLikeWildcards(queryString.replace(/^"(.*)"$/, '$1'));
     }
     if (Array.isArray(queryString) && typeof queryString[0] === 'string') {
-        return queryString[0].replace(/^"(.*)"$/, '$1');
+        return escapeLikeWildcards(queryString[0].replace(/^"(.*)"$/, '$1'));
     }
     return undefined
 }
-
+function escapeLikeWildcards(queryString: string) {
+    return queryString = queryString.replace(/[%_]/g, '\\$&');
+}
 export { formatQueryString };
