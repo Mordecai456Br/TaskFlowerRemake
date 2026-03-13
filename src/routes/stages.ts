732740fe3ed9ce126cb012dac1b728b491df4cad
Matheus Luna <matheusluna151634@gmail.com>
2026-03-13 17:51:16 -0300

    feat(stages): add new route for project stages management

---
+++ src/routes/stages.ts
@@ -0,0 +1,60 @@
+import express from 'express';
+import {and, eq} from "drizzle-orm";
+import { projects, stages } from "../database/schema";
+import {neonDatabase} from "../database";
+
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {  
+    console.log("1");
+    try {
+        const { title, description, order, projectId } = req.body;
+        if (!title && !description && !order && !projectId) {
+            return res.status(400).json({ error: 'Is required: title, description, order, projectId'});
+        }
+        const findProject = await neonDatabase 
+            .select()
+            .from(stages)
+            .where(and(
+                eq(stages.projectId, projectId),
+                eq(stages.title, title))
+            )
+            .limit(1)
+        const result = findProject[0];
+        console.log(findProject)
+        if (result) {
+            return res.status(409).json({ error: `You already created a stage with the title '${title}'`});
+        }
+        const [newStage] = await neonDatabase
+            .insert(stages)
+            .values({
+                title: title,
+                description: description,
+                order: order,
+                projectId: projectId
+            })
+            .returning();
+        return res.status(201).json(newStage);
+    } catch {
+        res.status(500).json();
+    }   
+});
+
+router.get('/', async (req, res) => {  
+    const findProject = await neonDatabase 
+        .select()
+        .from(stages)
+    const result = findProject
+    if (!result) {
+        return res.status(404).json({message: "vazio"})
+    }
+    return res.status(200).json(result)
+})
+
+export default router;
+
+
+
+
+
